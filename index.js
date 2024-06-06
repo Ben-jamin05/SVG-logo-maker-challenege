b718336d4f1ce605c2b98fba9121ef3b37670bc9
Ben Hill <benjaminjhill@icloud.com>
2024-06-05 20:03:23 -0500

    changed up some files and file purposes

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-const inquirer = require('inquirer');
+const userIn = require('./lib/userIn.js');
 
 function main() {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                message: "Please enter up to three characters for your logo",
-                name: "characters",
-            },
-            {
-                type: "input",
-                message: "Please enter a color or hexdecimal number for your desired color",
-                name: "color",
-            },
-            {
-                type: "list",
-                message: "Please select a shape for your logo",
-                name: "shape",
-                choices: [
-                    "Circle",
-                    "Triangle",
-                    "Square",
-                ],
-            },
-        ])
-        .then((data) => {
-            console.log(data);
-        });
+    userIn.userInput();
 }
 
 main();
